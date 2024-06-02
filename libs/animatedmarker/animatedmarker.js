12a54d8115e57b6222fd72fb03cf7be5f7ffd5a9
UgoGitSocial <ugo.padulosi@gmail.com>
2024-06-02 16:22:28 +0200

    Update animatedmarker.js

--- libs/animatedmarker/animatedmarker.js
+++ libs/animatedmarker/animatedmarker.js
@@ -1,8 +1,12 @@
 L.AnimatedMarker = L.Marker.extend({
   options: {
+    // meters
     distance: 200,
-    interval: 2000,
+    // ms
+    interval: 1000,
+    // animate on add?
     autoStart: true,
+    // callback onend
     onEnd: function(){},
     clickable: false
   },
@@ -12,12 +16,14 @@ L.AnimatedMarker = L.Marker.extend({
     L.Marker.prototype.initialize.call(this, latlngs[0], options);
   },
 
+  // Breaks the line up into tiny chunks (see options) ONLY if CSS3 animations
+  // are not supported.
   _chunk: function(latlngs) {
     var i,
         len = latlngs.length,
         chunkedLatLngs = [];
 
-    for (i=1; i<len; i++) {
+    for (i=1;i<len;i++) {
       var cur = latlngs[i-1],
           next = latlngs[i],
           dist = cur.distanceTo(next),
@@ -43,6 +49,7 @@ L.AnimatedMarker = L.Marker.extend({
   onAdd: function (map) {
     L.Marker.prototype.onAdd.call(this, map);
 
+    // Start animating when added to the map
     if (this.options.autoStart) {
       this.start();
     }
@@ -53,19 +60,23 @@ L.AnimatedMarker = L.Marker.extend({
         len = this._latlngs.length,
         speed = this.options.interval;
 
+    // Normalize the transition speed from vertex to vertex
     if (this._i < len && this._i > 0) {
       speed = this._latlngs[this._i-1].distanceTo(this._latlngs[this._i]) / this.options.distance * this.options.interval;
     }
 
+    // Only if CSS3 transitions are supported
     if (L.DomUtil.TRANSITION) {
       if (this._icon) { this._icon.style[L.DomUtil.TRANSITION] = ('all ' + speed + 'ms linear'); }
       if (this._shadow) { this._shadow.style[L.DomUtil.TRANSITION] = 'all ' + speed + 'ms linear'; }
     }
 
+    // Move to the next vertex
     this.setLatLng(this._latlngs[this._i]);
     this._i++;
 
-    this._tid = setTimeout(function() {
+    // Queue up the animation to the next next vertex
+    this._tid = setTimeout(function(){
       if (self._i === len) {
         self.options.onEnd.apply(self, Array.prototype.slice.call(arguments));
       } else {
@@ -74,29 +85,33 @@ L.AnimatedMarker = L.Marker.extend({
     }, speed);
   },
 
+  // Start the animation
   start: function() {
-    this._i = 0;
     this.animate();
   },
 
+  // Stop the animation in place
   stop: function() {
     if (this._tid) {
       clearTimeout(this._tid);
     }
   },
 
-  setLine: function(latlngs) {
+  setLine: function(latlngs){
     if (L.DomUtil.TRANSITION) {
+      // No need to to check up the line if we can animate using CSS3
       this._latlngs = latlngs;
     } else {
+      // Chunk up the lines into options.distance bits
       this._latlngs = this._chunk(latlngs);
       this.options.distance = 10;
       this.options.interval = 30;
     }
     this._i = 0;
   }
+
 });
 
-L.animatedMarker = function(latlngs, options) {
+L.animatedMarker = function (latlngs, options) {
   return new L.AnimatedMarker(latlngs, options);
 };
